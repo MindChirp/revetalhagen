308b6f9c2a8e4ff551a78e01364ff9675a57eff1
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-09-23 23:31:01 +0200

    Added new images

--- src/components/ui/about.tsx
+++ src/components/ui/about.tsx
@@ -16,15 +16,15 @@ import ReactiveCarousel, { ImageType } from "./reactive-carousel";
 const images: ImageType[] = [
   {
     alt: "Stol",
-    src: "/chair.jpg",
+    src: "/IMG_0610.jpg",
   },
   {
     alt: "Natur",
-    src: "/bg.jpg",
+    src: "/IMG_0714.jpg",
   },
   {
     alt: "Bryggerhuset",
-    src: "/bryggerhuset.jpg",
+    src: "/IMG_0860.jpg",
   },
   {
     alt: "Hage",
@@ -64,14 +64,14 @@ const About = ({ className, ...props }: AboutProps) => {
         <div className="w-fit flex mx-auto flex-col gap-20 px-10 md:px-20 max-w-[1050px] flex-0 items-center">
           <ReactiveCarousel images={images} />
           <AboutCard
-            img="/bryggerhuset.jpg"
-            alt="Bryggerhuset"
+            img="/IMG_0773.jpg"
+            alt="Kålhode"
             title="Hva vi gjør"
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a metus porttitor odio aliquam imperdiet at et augue. Cras nec aliquam orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus rutrum ipsum ut est tincidunt, eu imperdiet risus dapibus. Nunc imperdiet diam vitae felis maximus hendrerit. Praesent nunc eros, pellentesque sit amet mi eu, placerat viverra dui."
           />
           <AboutCard
             mirrored
-            img="/roar-holt-gard.jpg"
+            img="/IMG_0716.jpg"
             alt="Bryggerhuset"
             title="Hva vi står for"
             description="Etiam facilisis lorem leo, in auctor orci convallis vitae. Nunc blandit, sapien non interdum imperdiet, erat mauris ultrices est, at rutrum tortor diam ac lorem. Aliquam nec ultricies eros. Nulla in sollicitudin nibh, vitae venenatis nibh. Duis leo nisl, imperdiet sodales lacinia id, faucibus et nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur convallis dolor sit amet sem viverra tincidunt."
