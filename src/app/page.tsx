0ecf672f242a59da00917655dc58153df63a5ea8
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-10-15 17:28:07 +0200

    Added fun facts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import About from "@/components/ui/about/about";
 import Hero from "@/components/ui/hero/hero";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Suspense } from "react";
+import FrontpageLoader from "./components/frontpage-loader";
 
 export default function Home() {
   return (
     <main className="min-h-screen w-full max-w-[2000px] mx-auto overflow-hidden">
-      <div className="bg-white">
-        <Hero className="md:-mb-[200px]" displayBg />
-        <Suspense fallback={<Skeleton className="w-full h-40" />}>
+      <div className="bg-white min-h-screen">
+        <Suspense fallback={<FrontpageLoader />}>
+          <Hero className="md:-mb-[200px]" displayBg />
           <About className="z-10 relative" />
         </Suspense>
       </div>
